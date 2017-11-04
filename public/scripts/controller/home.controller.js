8a6d93d33fc61b5b7e32c87492e7d1a37842f851
Eric Friday <ericfriday@gmail.com>
2017-11-03 22:01:11 -0500

    refactored from ng-bind-html to class based object-pushing variant

--- public/scripts/controller/home.controller.js
+++ public/scripts/controller/home.controller.js
@@ -1,86 +1,107 @@
-myApp.controller('HomeController', function ($scope, $sce) {
+// myApp.controller('HomeController', function ($scope, $sce) { // remnant from trying to post questions to dom via sanitize
+myApp.controller('HomeController', function () {
     console.log('in home controller');
 
-    var vm = this;
+    // Code Readability notes:
+    // q = question
+    // a = answer
+    // sa = short answer
+    // mc = multiple choice
+    // ca = correct answer (for multiple choice)
+    // essay = essay (like SA, but longer char limit)
 
-    // the $sce.trustAsHtml is required to allow the functions or expressions to display the ngMaterial formatting/inputs.
-    vm.multipleChoiceTemplate = $sce.trustAsHtml(`
-    <div class="multipleChoiceQuestion">Multiple Choice:
-        <div>
-            <md-input-container><label>Question:</label>
-                <input ng-model="hc.quiz.data[i].question">
-            </md-input-container></div>
-            a)<md-input-container><label>Potential Answer 1</label>
-                <input ng-model="hc.quiz.data[i].potentialAnswer1">
-            </md-input-container>
-            b)<md-input-container><label>Potential Answer 2</label>
-                <input ng-model="hc.quiz.data[i].potentialAnswer2">
-            </md-input-container>
-            c) <md-input-container><label>Potential Answer 3</label>
-                <input ng-model="hc.quiz.data[i].potentialAnswer3">
-            </md-input-container>
-            d)<md-input-container><label>Potential Answer 4</label>
-                <input ng-model="hc.quiz.data[i].potentialAnswer4">
-            </md-input-container>
-        </div>
-    </div>`);
-    vm.shortAnswerTemplate = $sce.trustAsHtml(`
-    <div>Short Answer: 
-        <md-input-container><label>Question:</label>
-            <input ng-model="hc.quiz.data[i].question">
-        </md-input-container>
-        <md-input-container><label>Notes or Answer</label>
-            <input ng-model="hc.quiz.data[i].answer">
-        </md-input-container></div>`);
-    vm.essayTemplate = $sce.trustAsHtml(`
-    <div>Essay:
-        <md-input-container>
-            <label>Essay Title/Question</label>
-            <input ng-model="hc.quiz.data[i].question">
-        </md-input-container>
-        <md-input-container>
-                <label>Essay Notes & Answers</label>
-                <input ng-model="hc.quiz.data[i].answer">
-            </md-input-container>
-        <br>
-    </div>`);
-    vm.questionCounter = 0;
+    const vm = this;
+    vm.quiz = {data: []};
+    vm.name = '';
+    vm.tags = ['history', 'music'];
+    vm.questions = []; // holds questions to push into quiz
+    vm.currentMCQ = ''; // MC Question
+    vm.currentMCA1 = ''; // MC Answers
+    vm.currentMCA2 = '';
+    vm.currentMCA3 = '';
+    vm.currentMCA4 = '';
+    vm.currentMCCA = ''; // MC Correct Answer
+    vm.currentSAQ = ''; // SA Question
+    vm.currentSAA = ''; // SA Answer/hints to grade on
+    vm.currentEQ = ''; // Essay Question/Topic
+    vm.currentEA = ''; // Essay Answer/hints to grade on
 
-    // example of the quiz object holding various questions.
-    // quiz object to hold information and then be sent to router for processing & INSERT into db
-    vm.quiz = {
-        name: '',
-        tags: [],
-        data: [{
-            type: 'multipleChoice',
-            question: 'What did the fox say?',
-            potentialAnswer1: 'Meow',
-            potentialAnswer2: 'Bark',
-            potentialAnswer3: 'Neigh',
-            potentialAnswer4: 'NANANANANANANANANANA',
-            answer: 'NANANANANANANANANANA' // possibly have answer simply be A, B, C, D
-        },{
-            type: 'multipleChoice',
-            question: 'Imagine all the people, ________.',
-            potentialAnswer1: 'living in harmony',
-            potentialAnswer2: 'fighting in senseless wars',
-            potentialAnswer3: 'eating potatoes',
-            potentialAnswer4: '42',
-            answer: 'living in harmony' // possibly have answer simply be A, B, C, D
-        },{
-            type: 'shortAnswer',
-            question: 'Summarize the most recent Rick & Morty episode in two sentences or less.',
-            answer: 'It was a fantastic masterpiece of drama and comedy. Beth drank wine while R&M went on an adventure.'
-            // possibly have notes about what the instructor should look for instead of 'answer'?
-        },{
-            type: 'essay',
-            question: 'Why did the chicken cross the road?',
-            answer: 'To get to the other side.' // answer should simply be main points for the instructor to grade from
-        }]
+    // creates a multiple choice question to be inserted into questions array
+    class MCQuestion {
+        constructor (q, a1, a2, a3, a4, ca){
+            this.question = q;
+            this.ans1 = a1;
+            this.ans2 = a2;
+            this.ans3 = a3;
+            this.ans4 = a4;
+            this.correctAns = ca;
+            this.type = 'mc';
+        }
+    }
+
+    // pushes MC Q into Q array
+    vm.pushMCQ = (q, a1, a2, a3, a4, ca) => {
+        vm.newMCQuestion = new MCQuestion(q, a1, a2, a3, a4, ca);
+        vm.questions.push(vm.newMCQuestion);
+        console.log('loggin vm.newMCQuestion in pushMCQuestion -> ', vm.newMCQuestion);
+
+        // clear the form for the next questions submission
+        vm.currentMCQ = '';
+        vm.currentMCA1 = '';
+        vm.currentMCA2 = '';
+        vm.currentMCA3 = '';
+        vm.currentMCA4 = '';
+        vm.currentMCCA = '';
     };
 
-    vm.newMultipleChoice = () => {
-        console.log('newMultipleChoice() activated!! :)');
+    // creates a SA Q to be inserted into Q array
+    class ShortAnsQuestion {
+        constructor (q, a) {
+            this.question = q;
+            this.ans = a;
+            this.type = 'sa';
+        }
+    }
+
+    // pushes SA Q into Q array
+    vm.pushSAQ = (q, a) => {
+        vm.newSAQuestion = new ShortAnsQuestion(q, a);
+        vm.questions.push(vm.newSAQuestion);
+        console.log('logging vm.newSAQuestion in pushShortAnsQuestion -> ', vm.newSAQuestion);
+    };
+
+    // creates Essay question to be pushed into Q array
+    class Essay {
+        constructor (q, a) {
+            this.question = q;
+            this.ans = a;
+            this.type = 'essay';
+        }
+    }
+
+    // pushes Essay question into Q array
+    vm.pushEssayQ = (q, a) => {
+        vm.newEQ = new Essay(q, a);
+        vm.questions.push(vm.newEQ);
+        console.log('logging vm.newEQ in pushEssayQ -> ', vm.newEQ);
         
+    };
+
+    // creates overarching 'Test' item which holds name, tags, and questions.
+    // 'Test' item is equivalent to quiz.
+    // entire 'Test'/quiz will be sent to backend for insertion into DB.
+    class Test {
+        constructor (name, tags, q){
+            this.name = name; // name is to be a string
+            this.tags = tags; // tags will be an array of string
+            this.questions = q; // questions will be a class of either multiple choice, short answer, or essay. 
+                                        // questions will be an array of objects of the above questions.
+        }
+    }
+
+    vm.pushToQuiz = (name, tags, q) => {
+        vm.newQuiz = new Test(name, tags, q);
+        vm.quiz.data.push(vm.newQuiz);
+        console.log('logging vm.quiz in pushToquiz => ', vm.quiz);
     };
 });
